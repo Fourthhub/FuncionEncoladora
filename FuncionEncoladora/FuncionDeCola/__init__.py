6a5dd03ecd457b373fd628d31a872f9181011644
Diego Echaure <56819212+Fourthhub@users.noreply.github.com>
2024-03-25 13:46:13 +0100

    Add files via upload

---
+++ FuncionEncoladora/FuncionDeCola/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+import azure.functions as func
+import os, uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage, BinaryBase64DecodePolicy, BinaryBase64EncodePolicy
+
+#sdasda
+
+queue_name = "colaDeReservas"
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    
+    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        queue_client = QueueClient.from_connection_string(connect_str, queue_name)
+        queue_client.send_message(req)
+
+    except Exception as e:
+        logging.info("errrorr")
+
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
