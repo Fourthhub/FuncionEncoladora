6d48240d0f92e4be15670d7f0624973a23817c25
diego <diegoechaure@gmail.com>
2024-03-26 10:36:30 +0100

    fix

--- FuncionDeCola/__init__.py
+++ FuncionDeCola/__init__.py
@@ -6,21 +6,18 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage, BinaryBase64DecodePolicy, BinaryBase64EncodePolicy
 
 
-connect_str = "DefaultEndpointsProtocol=https;AccountName=funciondecola;AccountKey=RTl0R58hLPv0ZvsxDufY5jlw2s3yyFlDBC11fdzP1AXLMOo30GGxJ4vgjfcnj+2EMPR5YfWhYpSe+AStQPSFZw==;EndpointSuffix=core.windows.net"
+connect_str = "DefaultEndpointsProtocol=https;AccountName=colafun;AccountKey=9lrsrXglDMb2+9aY3V8uZzZbI36AXU1tVIc8QSpdmFRacuJeGJEZlU2IisrgZi2HNBzvbtuRc1x++AStbm3BaQ==;EndpointSuffix=core.windows.net"
 
-queue_name = "colaDeReservas"
+queue_name = "colafun"
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     
     logging.info('Python HTTP trigger function processed a request.')
-    try:
-        queue_client = QueueClient.from_connection_string(connect_str, queue_name)
-        queue_client.send_message(req)
+    queue_client = QueueClient.from_connection_string(connect_str, queue_name)
+    queue_client.send_message(req)
 
-    except Exception as e:
-        logging.info("errrorr")
 
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
